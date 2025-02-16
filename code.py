ae9c08957dea02437e66698f0ff1acbb1e883bf7
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2025-02-16 13:45:10 +0100

    Update code.py

--- code.py
+++ code.py
@@ -5,3 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 open_key = os.getenv("OPENAI_API_KEY") 
 OPEN_MODEL = "gpt-4-turbo"
+
+sys_prompt = 'You are an helpful customer service assistant, look through this website https://www.telfordsailingclub.co.uk and answer any questions \
+    a customer will have.'
