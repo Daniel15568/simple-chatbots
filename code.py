c9274f5508a9e8ed0a6e7f32b23cae2d814c07af
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2025-02-23 01:27:48 +0100

    Update code.py

--- code.py
+++ code.py
@@ -8,3 +8,21 @@ OPEN_MODEL = "gpt-4-turbo"
 
 sys_prompt = 'You are an helpful customer service assistant, look through this website https://gresford-sailing-club-gsc.github.io/index/ \
 and answer any questions a customer will have.'
+
+def chat(message, history):
+    messages = [{'role':'system', 'content':system}]
+    for user_message, assistant_message in history:
+        messages.append({'role':'user', 'content':user_message})
+        messages.append({'role':'assistant', 'content':assistant_message})
+    messages.append({'role':'user', 'content':message})
+    
+    print('History is: ')
+    print(history)
+    print('Message is: ')
+    print(message)
+
+    output = openai.chat.completions.create(model=OPEN_MODEL, messages=messages, stream=True)
+    stream = ''
+    for chunk in output:
+        stream += chunk.choices[0].delta.content or ''
+        yield stream
